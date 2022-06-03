6ab566e4e56adbb4079ebb85bd05ca54ee5e3414
AlehKhvasko <alehkhvasko@yahoo.com>
2022-06-03 00:31:14 -0500

    Testing application with Junit5 and Mockito

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,22 +1,63 @@
 package service;
 
-import org.junit.jupiter.api.BeforeEach;
+import models.User;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.mockito.Mockito;
+import repository.UserRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
-    @BeforeEach
-    void setUp() {
+    private final UserRepositoryImpl userRepository = Mockito.mock(UserRepositoryImpl.class);
+    private final UserService userService = new UserService(userRepository);
+
+    private User getUserAl(){
+        return User.builder()
+                .companyName("NorthStar")
+                .firstName("A")
+                .lastName("K")
+                .build();
+    }
+
+    private User getUserAnn(){
+        return User.builder()
+                .companyName("NorthStar")
+                .firstName("Ann")
+                .lastName("Tailor")
+                .build();
+    }
+    private User getUserDian(){
+        return User.builder()
+                .companyName("NorthStar")
+                .firstName("Dian")
+                .lastName("Princes")
+                .build();
     }
 
     @Test
     void createUser() {
+        //given
+        //when
+        userService.createUser(any());
+        //then
+        verify(userRepository, times(1)).createUser(any());
     }
 
     @Test
     void readAllUsers() {
+        //given
+        List<User> userList = new ArrayList<>();
+        userList.add(getUserAl());
+        //when
+        when(userRepository.readAllUsers()).thenReturn(userList);
+        List<User> userListfromDB = userService.readAllUsers();
+        //than
+        assertEquals(userList, userListfromDB);
     }
 
     @Test
